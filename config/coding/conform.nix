9e2ca6a94fef56abc897d9613abdd21e194308ec
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-03-06 17:40:24 -0600

    Installed conform

--- config/coding/conform.nix
+++ config/coding/conform.nix
@@ -1,4 +1,17 @@
 {
+  autoCmd = [
+    {
+      event = ["BufWritePre"];
+      pattern = ["*"];
+      callback = {
+        __raw = ''
+          function (args)
+            require("conform").format({ bufnr = args.buf })
+          end
+        '';
+      };
+    }
+  ];
   extraConfigLuaPost = ''
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   '';
@@ -17,13 +30,18 @@
   ];
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {};
     formatters.injected.options.ignore_errors = true;
     formattersByFt = {
+      bash = ["beautysh"];
+      css = ["stylint"];
+      html = ["djlint"];
+      just = ["just"];
       lua = ["stylua"];
       nix = ["alejandra"];
       php = ["phpcbf" "php_cs_fixer"];
-      python = ["isort" "black"];
+      python = ["isort" "black" "autoflake"];
+      "*" = ["auto_optional" "codespell" "trim_newlines" "trim_whitespace"];
+      "_" = ["injected"];
     };
   };
 }
