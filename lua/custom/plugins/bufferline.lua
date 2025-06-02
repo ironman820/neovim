00a760b41e4ac0ada18ef3b24a7d1f0104491488
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-06-02 04:26:07 -0500

    Restart from scratch
    Restarted deployment from scratch after leaving NixOS

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+return {
+  -- 'akinsho/bufferline.nvim',
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+  -- keys = {
+  --   {
+  --     '<leader>bd',
+  --     '<CMD>:bdelete<CR>',
+  --     desc = '[D]elete Buffer',
+  --   },
+  -- },
+  -- opts = {
+  --   options = {
+  --     diagnostics = 'nvim_lsp',
+  --     -- show_buffer_close_icons = false,
+  --     -- show_close_icon = false,
+  --     separator_style = 'slant',
+  --   },
+  -- },
+  -- lazy = false,
+}
