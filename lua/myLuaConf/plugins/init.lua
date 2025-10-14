1b8eb5f2da029903df0b21474b61493141e6604c
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-13 21:05:32 -0500

    update notify

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -13,6 +13,13 @@ if ok then
     on_open = function(win)
       vim.api.nvim_win_set_config(win, { focusable = false })
     end,
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
   })
   vim.notify = notify
   vim.keymap.set("n", "<Esc>", function()
