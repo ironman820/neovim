ff7fe1b26702ca58ce026a001ed1f20baf0a8fd3
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-11-17 17:46:22 -0600

    Migrated opts

--- lua/myLuaConf/init.lua
+++ lua/myLuaConf/init.lua
@@ -1,7 +1,3 @@
-
--- NOTE: various, non-plugin config
-require('myLuaConf.opts_and_keys')
-
 -- NOTE: register an extra lze handler with the spec_field 'for_cat'
 -- that makes enabling an lze spec for a category slightly nicer
 require("lze").register_handlers(require('nixCatsUtils.lzUtils').for_cat)
